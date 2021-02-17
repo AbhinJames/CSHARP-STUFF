439dbb30f691fd65e36bf7061c98d3954752d13a
Abhin Shiny James <abhinshub@gmail.com>
2021-02-17 17:21:40 -0500

    About Variables

--- LearningCSHARP/Program.cs
+++ LearningCSHARP/Program.cs
@@ -10,13 +10,17 @@ namespace LearningCSHARP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
-            
+            //string nameChoice = "Abhin";
+            //int ageChoice = 24;
 
-            Console.ReadLine();
+            //Console.WriteLine("There was once a man named {0}",nameChoice);
+            //Console.WriteLine("He was {0} years old",ageChoice);
+            //Console.WriteLine("He really liked the name {0}",nameChoice);
+            //Console.WriteLine("But didnt like being {0}",ageChoice);
+            //Console.WriteLine("Yet {0} was a happy {1} year old man", nameChoice,ageChoice);
+
+
+
         }
     }
 }
