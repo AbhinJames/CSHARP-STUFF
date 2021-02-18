abb2b43a360fee2261a8fd88f7ad2cfa7a1ad226
Abhin Shiny James <abhinshub@gmail.com>
2021-02-17 19:16:00 -0500

    A smalll mad lib game

--- LearningCSHARP/Program.cs
+++ LearningCSHARP/Program.cs
@@ -68,7 +68,27 @@ namespace LearningCSHARP
 
             //Console.WriteLine("The sum of {0} and {1} is {2}", numOne, numTwo, numSum);
 
-             
+            //MAD LIBS GAME
+
+            //string color, pluralNoun, celebrity;
+
+            //Console.WriteLine("Enter a color: ");
+            //color = Console.ReadLine();
+
+            //Console.WriteLine("Enter a plural noun: ");
+            //pluralNoun = Console.ReadLine();
+
+            //Console.WriteLine("Enter a celebrity name: ");
+            //celebrity = Console.ReadLine();
+
+            //Console.WriteLine("Roses are " + color);
+            //Console.WriteLine(pluralNoun + " are blue");
+            //Console.WriteLine("I love " + celebrity);
+
+
+
+
+
         }
     }
 }
