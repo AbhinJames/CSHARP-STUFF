351af985d74cf0ce3da92f82c1c8cc72221874f8
Abhin Shiny James <abhinshub@gmail.com>
2021-04-05 11:58:34 -0400

    -Database made in Server Explorer -Connection Added in Employee.cs -Query Syntax and Add button added

--- EmpoyeeManagementSystem/Employee.cs
+++ EmpoyeeManagementSystem/Employee.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace EmpoyeeManagementSystem
 {
@@ -15,6 +16,36 @@ namespace EmpoyeeManagementSystem
         public Employee()
         {
             InitializeComponent();
+        }
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\abhin\Documents\MyEmployeeDb.mdf;Integrated Security=True;Connect Timeout=30");
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            if(EmpIdTb.Text == "" || EmpNameTb.Text == "" || EmpPhoneTb.Text == "" ||EmpAddTb.Text == "")
+            {
+                MessageBox.Show("Missing Information");
+            }
+            else
+            {
+                try
+                {
+                    Con.Open();
+                    string query = "insert into EmployeeTbl values("+EmpIdTb.Text+"','"+EmpNameTb.Text+"','"+EmpAddTb.Text+"','"+EmpPosCb.SelectedItem.ToString()+"','"+EmpDOB.Value.Date+"','"+EmpPhoneTb.Text+"','"+EmpEduCb.SelectedItem.ToString()+"','"+EmpGenCb.SelectedItem.ToString()+"')";
+                    SqlCommand cmd = new SqlCommand(query, Con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Employee Added Succesfully");
+                    Con.Close();
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+            }
+        }
+
+        private void EmpEduCb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
