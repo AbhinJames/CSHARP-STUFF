70db93cdb2cd91114e1bb3ad4dd292aad69ac153
Abhin Shiny James <abhinshub@gmail.com>
2021-05-03 19:50:42 -0400

    Add button, Delete btn,Edit btn functionalities added

--- EmpoyeeManagementSystem/Employee.cs
+++ EmpoyeeManagementSystem/Employee.cs
@@ -71,5 +71,74 @@ namespace EmpoyeeManagementSystem
         {
             populate();
         }
+
+        private void bunifuThinButton23_Click(object sender, EventArgs e)
+        {
+            if (EmpIdTb.Text == "")
+            {
+                MessageBox.Show("Enter an Employee ID");
+            }
+            else
+            {
+                try
+                {
+                    Con.Open();
+                    string query = "delete from EmployeeTbl where EmpId='" + EmpIdTb.Text + "';";
+                    SqlCommand cmd = new SqlCommand(query, Con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Employee Deleted Successfully");
+                    Con.Close();
+                    populate();
+                }
+                catch (Exception Ex)
+                {
+
+                    MessageBox.Show(Ex.Message);
+                }
+            }
+        }
+
+        private void EmpDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            EmpIdTb.Text = EmpDGV.SelectedRows[0].Cells[0].Value.ToString();
+            EmpNameTb.Text = EmpDGV.SelectedRows[0].Cells[1].Value.ToString();
+            EmpAddTb.Text = EmpDGV.SelectedRows[0].Cells[2].Value.ToString();
+            EmpPosCb.Text = EmpDGV.SelectedRows[0].Cells[3].Value.ToString();
+            EmpDOB.Text = EmpDGV.SelectedRows[0].Cells[4].Value.ToString();
+            EmpPhoneTb.Text = EmpDGV.SelectedRows[0].Cells[5].Value.ToString();
+            EmpEduCb.Text = EmpDGV.SelectedRows[0].Cells[6].Value.ToString();
+            EmpGenCb.Text = EmpDGV.SelectedRows[0].Cells[7].Value.ToString();
+            
+            
+        }
+
+        private void bunifuThinButton22_Click(object sender, EventArgs e)
+        {
+            if (EmpIdTb.Text == "" || EmpNameTb.Text == "" || EmpPhoneTb.Text == "" || EmpAddTb.Text == "")
+            {
+                MessageBox.Show("Missing Information");
+            }
+            else
+            {
+                try
+                {
+                    Con.Open();
+                    string query = "update EmployeeTbl set EmpName='"+EmpNameTb.Text+"',EmpAdd='"+EmpAddTb.Text+"',EmpPos='"+EmpPosCb.SelectedItem.ToString()+"',EmpDOB='"+EmpDOB.Value.Date+"',EmpPhone='"+EmpPhoneTb.Text+"',EmpEdu='"+EmpEduCb.SelectedItem.ToString()+"',EmpGen='"+EmpGenCb.SelectedItem.ToString()+"'where EmpId='"+EmpIdTb.Text+"';";
+                    SqlCommand cmd = new SqlCommand(query, Con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Employee Updated Successfully");
+                    Con.Close(); 
+                    populate();
+
+                }
+                catch (Exception Ex)
+                {
+
+                    MessageBox.Show(Ex.Message);
+                }
+            }
+
+
+        }
     }
 }
